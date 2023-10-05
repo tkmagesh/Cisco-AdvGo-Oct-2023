3e7917dcdceb7a3542aadf5e4c56996c9badd5bc
Magesh Kuppan <tkmagesh77@gmail.com>
2023-10-05 12:22:44 +0530

    timeout based cancellation

--- 04-context/01-basics.go
+++ 04-context/01-basics.go
@@ -10,14 +10,33 @@ import (
 func main() {
 	wg := &sync.WaitGroup{}
 	rootCtx := context.Background()
-	cancelCtx, cancel := context.WithCancel(rootCtx)
+	// explicit cancellation
+	/*
+		ctx, cancel := context.WithCancel(rootCtx)
+		fmt.Println("Hit ENTER to stop...")
+		go func() {
+			fmt.Scanln()
+			cancel()
+		}()
+	*/
+
+	// time-based cancellation
+	/*
+		ctx, cancel := context.WithTimeout(rootCtx, time.Second*10)
+		defer cancel()
+	*/
+
+	// combining explicit and time-based cancellation
+	ctx, cancel := context.WithTimeout(rootCtx, time.Second*10)
+	defer cancel()
 	fmt.Println("Hit ENTER to stop...")
 	go func() {
 		fmt.Scanln()
 		cancel()
 	}()
+
 	wg.Add(1)
-	go fn(cancelCtx, wg)
+	go fn(ctx, wg)
 	wg.Wait()
 	fmt.Println("Done...!")
 }
