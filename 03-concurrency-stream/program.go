7cbb5e5ca1bbd033ea5300d207151420a67841cd
Magesh Kuppan <tkmagesh77@gmail.com>
2023-10-05 09:35:51 +0530

    concurrency-stream - partial solution

---
+++ 03-concurrency-stream/program.go
@@ -0,0 +1,80 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+	"strconv"
+	"sync"
+)
+
+func main() {
+	fileWg := &sync.WaitGroup{}
+	processWg := &sync.WaitGroup{}
+
+	dataCh := make(chan int)
+	oddCh := make(chan int)
+	evenCh := make(chan int)
+
+	fileWg.Add(1)
+	go Source("data1.dat", dataCh, fileWg)
+	fileWg.Add(1)
+	go Source("data2.dat", dataCh, fileWg)
+
+	processWg.Add(1)
+	go Splitter(dataCh, evenCh, oddCh, processWg)
+
+	processWg.Add(1)
+	go Sum("Even Total :", evenCh, processWg)
+
+	processWg.Add(1)
+	go Sum("Odd Total :", oddCh, processWg)
+
+	fileWg.Wait()
+	close(dataCh)
+	processWg.Wait()
+}
+
+func Source(fileName string, ch chan int, wg *sync.WaitGroup) {
+	defer wg.Done()
+	file, err := os.Open(fileName)
+	if err != nil {
+		log.Fatalln(err)
+	}
+	defer file.Close()
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		val := scanner.Text()
+		if no, err := strconv.Atoi(val); err == nil {
+			ch <- no
+		}
+	}
+}
+
+func Splitter(ch chan int, evenCh chan int, oddCh chan int, wg *sync.WaitGroup) {
+	defer wg.Done()
+	defer close(evenCh)
+	defer close(oddCh)
+	counter := 0
+	for data := range ch {
+		counter += 1
+		if data%2 == 0 {
+			evenCh <- data
+		} else {
+			oddCh <- data
+		}
+	}
+	fmt.Println("counter :", counter)
+}
+
+func Sum(title string, ch chan int, wg *sync.WaitGroup) {
+	defer wg.Done()
+	total := 0
+	count := 0
+	for val := range ch {
+		total += val
+		count += 1
+	}
+	fmt.Println(title, "total ", total, "count :", count)
+}
